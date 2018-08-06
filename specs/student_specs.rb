18dbf42c6d85958da29f44f2b652939186ad6bda
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-06 15:01:36 +0100

    get_cohort created

--- specs/student_specs.rb
+++ specs/student_specs.rb
@@ -8,4 +8,8 @@ def test_get_student_name
   assert_equal("Filip", student.get_name)
 end
 
+def test_get_student_cohort
+  student = Student.new("Filip", "E24")
+  assert_equal("E24", student.get_cohort)
+end
 end
