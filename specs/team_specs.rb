93d84bf468232d548752d95c57050a1dd99bf32b
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-06 15:59:06 +0100

    Refactor created

--- specs/team_specs.rb
+++ specs/team_specs.rb
@@ -6,26 +6,27 @@ class TeamTest < MiniTest::Test
 
   def test_get_team_name
     team = Team.new("Team1", ["Filip", "Ben"], "Craig")
-    assert_equal("Team1", team.get_team_name)
+      assert_equal("Team1", team.team_name)
+    # assert_equal("Team1", team.get_team_name)
   end
+
+
 
   def test_get_team_players
     team = Team.new("Team1", ["Filip", "Ben"], "Craig")
-    assert_equal(["Filip", "Ben"], team.get_team_players)
-  end
-
-  def test_get_team_players
-    team = Team.new("Team1", ["Filip", "Ben"], "Craig")
-    assert_equal(["Filip", "Ben"], team.get_team_players)
+    assert_equal(["Filip", "Ben"], team.players)
+    # assert_equal(["Filip", "Ben"], team.get_team_players)
   end
   def test_get_team_coach
     team = Team.new("Team1", ["Filip", "Ben"], "Craig")
-    assert_equal("Craig", team.get_team_coach)
+    assert_equal("Craig", team.coach)
+    # assert_equal("Craig", team.get_team_coach)
   end
   def test_change_coach
     team = Team.new("Team1", ["Filip", "Ben"], "Craig")
-    team.change_coach("Jose")
-    assert_equal("Jose", team.get_team_coach)
+    team.coach = ("Jose")
+    assert_equal("Jose", team.coach)
+    # assert_equal("Jose", team.get_team_coach)
   end
 
 end
--- team.rb
+++ team.rb
@@ -1,24 +1,24 @@
 class Team
+  attr_accessor(:team_name, :players, :coach)
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
   end
 
-  def get_team_name()
-    return @team_name
-  end
-
-  def get_team_players()
-    return @players
-  end
-
-  def get_team_coach()
-    return @coach
-  end
-
-  def change_coach(new_coach)
-    @coach = new_coach
-
-  end
+  # def get_team_name()
+  #   return @team_name
+  # end
+  #
+  # def get_team_players()
+  #   return @players
+  # end
+  #
+  # def get_team_coach()
+  #   return @coach
+  # end
+  #
+  # def change_coach(new_coach)
+  #   @coach = new_coach
+  # end
 end
